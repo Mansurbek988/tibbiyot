bff3b5ed8fd370697762e878061dbb8b6385ba7d
Mansurbek988 <mansurbek988@github.com>
2026-03-17 08:58:57 +0500

    Fix backend import error in appointment schema

--- backend/app/schemas/appointment.py
+++ backend/app/schemas/appointment.py
@@ -2,7 +2,7 @@ from typing import Optional, List
 from pydantic import BaseModel
 from backend.app.db.models import AppointmentStatus
 from datetime import datetime
-from backend.app.schemas.user import User, UserCreate
+from backend.app.schemas.user import User, UserCreate, UserUpdate
 
 # --- Doctor Schemas ---
 class DoctorBase(BaseModel):
